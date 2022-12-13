316c4e819f6f49e9c086f889ae4e3d672b3d9ede
Doug Melton <dmelton@gopro.com>
2022-12-12 17:40:29 -0800

    Updated MQTT address

--- helen-particle/helen.ino
+++ helen-particle/helen.ino
@@ -31,7 +31,7 @@ int pin_right_down = D5;
 //----------------
 
 char myIpString[24];
-byte server[] = { 10, 5, 23, 6 };
+byte server[] = { 10, 5, 22, 2 };
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
     char p[length + 1];
